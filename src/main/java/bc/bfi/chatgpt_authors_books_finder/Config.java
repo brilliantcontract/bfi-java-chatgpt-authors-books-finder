0684b03ae942ad01ddd621df102659e6faed9f8b
bob <brilliantcontract@yahoo.com>
2025-10-06 13:59:56 +0000

    Add postgresql support to porject.

--- src/main/java/bc/bfi/chatgpt_authors_books_finder/Config.java
+++ src/main/java/bc/bfi/chatgpt_authors_books_finder/Config.java
@@ -2,14 +2,15 @@ package bc.bfi.chatgpt_authors_books_finder;
 
 public final class Config {
 
-    public static final String DB_HOST = "3.17.216.88";
-    public static final int DB_PORT = 3306;
-    public static final String DB_NAME = "chatgpt_authors_books_finder";
-    public static final String DB_USER = "root";
-    public static final String DB_PASSWORD = "RootSecret1!";
-    public static final String DB_TABLE = "authors";
-    public static final String DB_URL =
-            "jdbc:mysql://" + DB_HOST + ":" + DB_PORT + "/" + DB_NAME + "?useUnicode=true&characterEncoding=UTF-8&serverTimezone=UTC";
+    /**
+     * Database.
+     */
+    static final String DB_HOST = "3.140.167.34";
+    static final String DB_PORT = "5432";
+    static final String DB_USERNAME = "redash";
+    static final String DB_PASSWORD = "te83NECug38ueP";
+    static final String DB_DATABASE = "scrapers";
+    static final String DB_TABLE = "chatgpt_websites_finder.chatgpt_authors";
 
     public static final String SERVICE_BASE_URL_ENV = "SERVICE_BASE_URL";
     public static final String BASE_URL = "http://localhost:8080";
